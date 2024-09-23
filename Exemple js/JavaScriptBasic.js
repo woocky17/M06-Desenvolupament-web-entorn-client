7f752d647a8b3cc0a6ae7f77fb674696f3e1a150
Woocky17 <davidortetsabater@gmail.com>
2024-09-23 19:11:49 +0200

    Creation of the javaScript exemple

---
+++ Exemple js/JavaScriptBasic.js
@@ -0,0 +1,9 @@
+console.log("Hola");
+let div = window.document.getElementById("div_text");
+div.innerHTML = "Hola HTML";
+div.innerText += " Patata";
+//alert ("PopUp");
+
+let numero = window.prompt("Indica un numero");
+document.getElementById("resultat")
+resultat.innerText=numero;
